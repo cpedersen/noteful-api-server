0fbd2a6cc76cce3356479924a74b208c9aa65c98
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-01-19 20:04:45 -0500

    Bring back error handler code in app.js that I commented out

--- src/app.js
+++ src/app.js
@@ -68,12 +68,12 @@ app.get('/', (req, res) => {
 /* -------------------------------------------------------- */
 app.use(function errorHandler(error, req, res, next) {
     let response
-    //if (NODE_ENV === 'production') {
-    //  response = { error: { message: 'server error' } }
-    //} else {
+    if (NODE_ENV === 'production') {
+      response = { error: { message: 'server error' } }
+    } else {
       console.error(error)
       response = { message: error.message, error }
-    //}
+    }
     console.log(response, error)
     res.status(500).json(response)
 })
